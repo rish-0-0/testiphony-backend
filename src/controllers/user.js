85f966aaaf24651f81b3a9d36c5bd23bee9d020e
Rishabh Anand <rishab@caarya.in>
2020-10-13 19:48:03 +0530

    added more markups  On branch master  Changes to be committed: 	modified:   src/controllers/user.js

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -16,3 +16,51 @@ module.exports.loginUser = async (req, res) => {
     
   }
 }
+
+module.exports.subscribeUser = async (req, res) => {
+  try {
+    
+  } catch (e) {
+    
+  }
+}
+
+module.exports.rankUser = async (req, res) => {
+  try {
+
+  } catch (e) {
+    
+  }
+};
+
+module.exports.queryUsers = async (req, res) => {
+  try {
+
+  } catch (e) {
+    
+  }
+};
+
+module.exports.logoutUser = async (req, res) => {
+  try {
+    
+  } catch (e) {
+    
+  }
+};
+
+module.exports.getUser = async (req, res) => {
+  try {
+    
+  } catch (e) {
+    
+  }
+};
+
+module.exports.deleteUser = async (req, res) => {
+  try {
+    
+  } catch (e) {
+    
+  }
+};
